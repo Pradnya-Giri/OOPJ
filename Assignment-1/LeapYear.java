9b67458d35599374e3b70d77989940f26d4a6d0e
Pradnya-Giri <163002106+Pradnya-Giri@users.noreply.github.com>
2024-03-14 23:32:23 +0530

    Update LeapYear.java

--- Assignment-1/LeapYear.java
+++ Assignment-1/LeapYear.java
@@ -4,9 +4,15 @@ class LeapYear{
 public static void main(String[] args){
 	Scanner sc= new Scanner(System.in);
 	int year= sc.nextInt();
-	boolean isLeapYear= true;
+	//boolean isLeapYear= true;
+	
+	String result;
+	result = ((year%4 == 0 && year %100 != 0)? "is a leap year." : (year%400 == 0) ? "is a leap year." : "is not a leap year.");
 
-	if(year%4==0){
+	System.out.println(year + " " + result);
+	sc.close();
+
+	/* if(year%4==0){
 		isLeapYear= true;
 		if(year%100==0){
 		if(year%400==0){
@@ -18,7 +24,7 @@ public static void main(String[] args){
 	else{
 		isLeapYear= false;
 	}
-
+*/
 	if(isLeapYear){System.out.println("Leap year!!");}
 	else{System.out.println("Not a Leap year.");}
 
